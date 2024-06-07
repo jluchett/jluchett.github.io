aaccd1d81f8f5a120b3d3b0703afdbf6ebf99bae
jluchett <jluchett@hotmail.com>
2024-06-07 10:30:31 -0500

    Configurando despliegue con GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/jluchett.github.io.git/', // Reemplaza 'nombre-del-repositorio' con tu nombre de repo
 })
