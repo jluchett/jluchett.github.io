d07ce3e5fcdbdac6ccffdb9597318902ec170193
jluchett <jluchett@hotmail.com>
2024-06-07 11:56:00 -0500

    Mejorando configuracion GitHub actions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Reemplaza 'nombre-del-repositorio' con tu nombre de repo
+  base: '/', 
 })
